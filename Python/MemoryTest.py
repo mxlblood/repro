495cc6bd473e06d476b120dcd0ced0278db980b4
Eduardo Perez <ed.perez@icloud.com>
2017-10-20 12:39:48 -0500

    added the prototype MemoryTest.py module

--- Python/MemoryTest.py
+++ Python/MemoryTest.py
@@ -1,2 +1,30 @@
 # MemoryTest.py
+#
 # Author: Eddie
+# Simple script that performs a memory test on the SUT hardware
+# This test determines if the program can produce a list with one million items
+# Prints SUCCESS if successful, FAIL if not
+#
+# This program will run on a SUT
+
+import random
+
+from Python.modules import valueList
+
+
+def generateList(someList):
+    count = 0
+    while (count<valueList.MEMTEST_MAX):
+        someList.append(random.randint(0,9))
+        count+=1
+    return someList
+
+
+def main():
+    memList = []
+    memList = generateList(memList)
+    if len(memList) > 0:
+        print("SUCCESS")
+    else:
+        print("FAIL")
+        raise Exception
